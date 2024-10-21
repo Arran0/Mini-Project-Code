6ba03d3632d73b9b01058b78d8e67d564c288ebe
Arran <arranamaravel@gmail.com>
2024-10-21 21:04:28 +0800

    Add files via upload

---
+++ code3_miniproject.py
@@ -0,0 +1,22 @@
+'''The "Introduction to Data Science" is a Year 1 course offered at NTU. This course attracts a
+diverse group of students from various disciplines due to its widespread applicability and
+popularity.
+Recently, the course has experienced a significant increase in enrollment, with 6,000
+students registered. These students are organized into 120 tutorial groups, each consisting
+of 50 students. The course coordinator is facing challenges in efficiently forming teams for
+a mini-project component of the course.
+To address this issue, the course coordinator seeks your expertise in developing an
+application capable of organizing students into teams of five for the data science miniproject. You are provided with a csv file (records.csv) consist of 6,000 student records with
+their Tutorial Group, Student ID, Name, School, Gender, and CGPA. The application must
+ensure fairness and diversity when forming teams by considering the following factors:
+1. School Affiliation: To ensure a mix of knowledge and skills, no team should have a
+majority of students from the same school.
+2. Gender: To promote gender diversity, no team should have a majority of students
+from the same gender.
+3. Current CGPA: To balance academic performance, teams should not consist
+predominantly of students with very high or very low CGPAs.
+The objective of your program is to strive for balanced and diverse team compositions,
+taking into account the aforementioned criteria. Some tolerant are acceptable if happen
+that the tutorial group are dominated by student from the same background or profile.'''
+
+print("This is the mini project code")
