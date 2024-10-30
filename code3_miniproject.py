0195f7c845485a7c7e6b5d66903fa2642ce877a3
Arran <arranamaravel@gmail.com>
2024-10-30 15:31:40 +0800

    Update code3_miniproject.py

--- code3_miniproject.py
+++ code3_miniproject.py
@@ -1,22 +1,40 @@
-'''The "Introduction to Data Science" is a Year 1 course offered at NTU. This course attracts a
-diverse group of students from various disciplines due to its widespread applicability and
-popularity.
-Recently, the course has experienced a significant increase in enrollment, with 6,000
-students registered. These students are organized into 120 tutorial groups, each consisting
-of 50 students. The course coordinator is facing challenges in efficiently forming teams for
-a mini-project component of the course.
-To address this issue, the course coordinator seeks your expertise in developing an
-application capable of organizing students into teams of five for the data science miniproject. You are provided with a csv file (records.csv) consist of 6,000 student records with
-their Tutorial Group, Student ID, Name, School, Gender, and CGPA. The application must
-ensure fairness and diversity when forming teams by considering the following factors:
-1. School Affiliation: To ensure a mix of knowledge and skills, no team should have a
-majority of students from the same school.
-2. Gender: To promote gender diversity, no team should have a majority of students
-from the same gender.
-3. Current CGPA: To balance academic performance, teams should not consist
-predominantly of students with very high or very low CGPAs.
-The objective of your program is to strive for balanced and diverse team compositions,
-taking into account the aforementioned criteria. Some tolerant are acceptable if happen
-that the tutorial group are dominated by student from the same background or profile.'''
+labels = ["Tutorial Group","Student ID","School","Name","Gender","CGPA"]
 
-print("This is the mini project code")
+with open('./records.csv', mode ='r') as file:    
+    full_list = [] #entire file, list of each records
+    next(file) #skips heading line
+    for lines in file:
+        line = {} #each record as dictionary
+        lines = lines.rstrip() #each record as csv
+        lines = lines.split(",") #makes list out of csv files
+        for i in range(len(lines)):
+            if labels[i] == "CGPA":
+                line[labels[i]] = float(lines[i])
+            else:
+                line[labels[i]] = lines[i]
+        full_list.append(line)
+
+def sortTut(list):
+    tutgrp = {}
+    for i in list:
+        sum_gpa = 0
+        if i["Tutorial Group"] in tutgrp:
+            continue
+        else:
+            tut_list = [z for z in list if z["Tutorial Group"] == i["Tutorial Group"]]
+            schl_list, cgpa = sortSchool(tut_list)
+            sum_gpa += cgpa
+            tutgrp[i["Tutorial Group"]] = [schl_list, round(sum_gpa/len(tut_list),2)]
+    return tutgrp
+sorted_list = sortTut(full_list)
+
+def sortGender(list): #is it full_list
+    gender = {}
+    sum_gpa = 0
+    for i in list:
+        sum_gpa += i["CGPA"]
+        if i["Gender"] in gender:
+            continue
+        else:
+            gender[i["Gender"]] = [z for z in list if z["Gender"] == i["Gender"]]
+    return gender, sum_gpa
